8ea6239b9a50fec155003d129a5505b3fd85e45d
Droffilc-codes <cliffordchisom@gmail.com>
2024-12-01 17:00:09 +0100

    20 update for render

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,7 +39,7 @@ app.get('*', (req, res)=>{
     res.send(indexFilePath)
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
     console.log('Listening on port ' + PORT)
